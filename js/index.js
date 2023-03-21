70fcba5d69cda36c7fde994acd605b2d9a8c5790
Armen Shadoyan <armenshadoyan2022@gmail.com>
2023-03-21 17:55:40 +0300

    second commit, added some functional

--- js/index.js
+++ js/index.js
@@ -23,10 +23,11 @@ logoBlock.classList.add("logo-block");
 logoImg.classList.add("logo-bmg");
 logoImg.src = "/imgs/logo-large.png";
 inputsBlock.classList.add("inputs-block");
-loginInput.classList.add("login-input","active-login","input-block-item");
-passwordInput.classList.add("password-input","input-block-item");
+loginInput.classList.add("login-input","active-login","input-block-item","input");
+passwordInput.classList.add("password-input","input-block-item","input");
 loginInput.placeholder = "Username";
 passwordInput.placeholder = "Password";
+passwordInput.type = "password";
 loginBtn.classList.add("login-btn","input-block-item");
 loginBtn.textContent = "Log In";
 errorMessage.textContent = "invalid login or password";
@@ -74,8 +75,10 @@ function printKeyboard(keyboardKeys){
 			if(key.toUpperCase() != key.toLowerCase() && key.length === 1){
 				symbol.classList.add("letter");
 			}
-			if(key === "Done" || key === "clean"){
-				symbol.classList.add("bigKeys");
+			if(key === "Done"){
+				symbol.classList.add("bigKeys","done");
+			}else if( key === "clean"){
+				symbol.classList.add("bigKeys","clean");
 			}else if(key === " "){
 				symbol.classList.add("space");
 			}else if( key === "123" || key === "Eng"){
@@ -92,6 +95,7 @@ function printKeyboard(keyboardKeys){
 				symbol.textContent = key;
 			}
 			if(key === "back"){
+				symbol.classList.add("back");
 				symbol.innerHTML = `
 				<span class="material-symbols-outlined back-space">
 						backspace
@@ -106,33 +110,30 @@ function printKeyboard(keyboardKeys){
 
 	});
 }
-
-function activeKey(){
-	const keys = document.querySelectorAll(".key");
-	const rows = document.querySelectorAll(".row");
+let keyIndex = 0;
+let rowIndex = 0;
 	
-	let keyIndex = 0;
-	let rowindex = 0;
-
-	rows[0].querySelectorAll(".key")[0].classList.add("active");
-	const activeKey = document.querySelector(".active");
-
+	// document.querySelectorAll(".row")[0][0].classList.add("active");
+	
 	document.addEventListener("keydown",(e) => {
-		// let rowkeys = rows[c].querySelectorAll(".key");
-		let rowkeys = rows[rowindex].querySelectorAll(".key");
-
+		const rows = document.querySelectorAll(".row");
+		
+		
+		let rowkeys = rows[rowIndex].querySelectorAll(".key");
 		if(e.key === "ArrowRight" ){
+			
 
 			if(keyIndex === rowkeys.length-1){
 				keyIndex = -1;
 				if(document.querySelector(".active"))document.querySelector(".active").classList.remove("active");
 			}
-
+			
 			keyIndex++;
 			rowkeys[keyIndex].classList.add("active");
 			if(rowkeys[keyIndex-1]){
 				rowkeys[keyIndex-1].classList.remove("active");
 			}
+
 		 }else if(e.key === "ArrowLeft"){
 			if(keyIndex === 0){
 				keyIndex = rowkeys.length;
@@ -144,22 +145,22 @@ function activeKey(){
 			if(rowkeys[keyIndex+1])rowkeys[keyIndex+1].classList.remove("active");
 		 }
 		 
-		 if(e.key === "ArrowDown" && rowindex < 3){
+		 if(e.key === "ArrowDown" && rowIndex < 3){
 
 			rowkeys[keyIndex].classList.remove("active");
-			rowindex++;	
-			rowkeys = rows[rowindex].querySelectorAll(".key");
+			rowIndex++;	
+			rowkeys = rows[rowIndex].querySelectorAll(".key");
 
 			if(keyIndex > rowkeys.length-1)keyIndex = rowkeys.length-1;
 			
 
 			rowkeys[keyIndex].classList.add("active");	
 
-		 }else if(e.key === "ArrowUp" && rowindex > 0){
+		 }else if(e.key === "ArrowUp" && rowIndex > 0){
 			rowkeys[keyIndex].classList.remove("active");
-			rowindex --;
-			rowkeys = rows[rowindex].querySelectorAll(".key");
-			if(rowindex === 2){
+			rowIndex --;
+			rowkeys = rows[rowIndex].querySelectorAll(".key");
+			if(rowIndex === 2){
 				if(keyIndex === 2){
 					keyIndex = rowkeys.length-1;
 				}else if (keyIndex === 0){
@@ -173,14 +174,20 @@ function activeKey(){
 
 	});
 
-}
+
 
 function activeInputs(){
 	const inputs = document.querySelectorAll(".input-block-item");
 	let i = 0;
-document.addEventListener("keydown",(e) => {
+
+	document.addEventListener("keydown",(e) => {
 	if(e.key === "Enter" && !loginBtn.classList.contains("active-login")){
 		keyboard.style.display = "flex";
+		
+		if(!document.querySelector(".active")){
+		document.querySelectorAll(".row")[0].querySelectorAll(".key")[0].classList.add("active");
+		}
+		
 	}else if(e.key === "Backspace"){
 		keyboard.style.display = "none";
 	}
@@ -192,13 +199,14 @@ document.addEventListener("keydown",(e) => {
 		}, 3000);
 	}
 
-
 	if(e.key === "ArrowDown" && keyboard.style.display === "none"){
+		loginBtn.classList.remove("active-login-btn");
 		inputs[i].classList.remove("active-login");
 		i++;
 		if(i > 2)i = 0;
 		inputs[i].classList.add("active-login");
 	}else if(e.key === "ArrowUp" && keyboard.style.display === "none"){
+		loginBtn.classList.remove("active-login-btn");
 		inputs[i].classList.remove("active-login");
 		if(i === 0)i = inputs.length;
 		i--;
@@ -214,12 +222,15 @@ function createNumbersKeyboard(){
 			document.querySelectorAll(".numbers-key")[1].classList.contains("active")){
 						if(document.querySelectorAll(".numbers-key")[0].textContent === "123"){
 							keyboard.innerHTML = "";
-							printKeyboard(numbersKeyboard);				
+							printKeyboard(numbersKeyboard);	
+							keyIndex = 0;
+							rowIndex = 0;
 						}else{
 							keyboard.innerHTML = "";
-							printKeyboard(lettersKeyboard);				
+							printKeyboard(lettersKeyboard);	
+							keyIndex = 0;
+							rowIndex = 0;
 						}
-						activeKey();
 			}
 		}
 	});
@@ -231,7 +242,6 @@ function upLow(){
 		const letters = document.querySelectorAll(".letter");
 		if(e.key === "Enter"){
 			if(shift[0].classList.contains("active") || shift[1].classList.contains("active")){
-				console.log(1);
 				letters.forEach(item => {
 					if(item.textContent !== item.textContent.toUpperCase()){
 					item.textContent = item.textContent.toUpperCase();
@@ -245,9 +255,190 @@ function upLow(){
 	});
 }
 
+function activeInputOnclick(){
+	const inputs = document.querySelectorAll(".input");
+	inputs.forEach((item,index) => {
+		item.addEventListener("click",() => {
+			if(loginBtn.classList.contains("active-login-btn")){
+				loginBtn.classList.remove("active-login-btn");
+			}
+
+			if(index === 0){
+				loginBtn.classList.remove("active-login");
+				item.classList.add("active-login");
+				if(inputs[1].classList.contains("active-login")){
+					inputs[1].classList.remove("active-login");
+				}
+			}else if(index === 1){
+				loginBtn.classList.remove("active-login");
+				item.classList.add("active-login");
+				if(inputs[0].classList.contains("active-login")){
+					inputs[0].classList.remove("active-login");
+				}
+			}
+			keyboard.style.display = "flex";
+			
+		});
+	});
+
+	loginBtn.addEventListener("click",() => {
+		inputs.forEach(item => {
+			item.classList.remove("active-login");
+		});
+
+		loginBtn.classList.add("active-login-btn");
+
+		errorMessage.style.display = "block";
+		setTimeout(() => {
+			errorMessage.style.display = "none";
+		}, 3000);
+	});	
+}
+
+function print(){
+	document.addEventListener("keydown",(e)=>{
+		const activeInput = document.querySelector(".active-login");
+		const pressedKey = document.querySelector(".active");
+		if(e.key === "Enter"){		
+			if(pressedKey){
+				if(!pressedKey.classList.contains("shift") && !pressedKey.classList.contains("back")
+				&& pressedKey.textContent.length === 1){
+
+				activeInput.value += pressedKey.textContent;
+
+			}
+			}
+			
+		}
+	});
+}
+
+function deleteeOnKey(){
+	document.addEventListener("keydown",(e) => {
+		const activeInput = document.querySelector(".active-login");
+		const backBtn = document.querySelector(".back");
+		const cleanBtn = document.querySelector(".clean");
+		const doneBtn = document.querySelector(".done");
+		if(e.key === "Enter" && backBtn.classList.contains("active")){
+			activeInput.value = activeInput.value.split("").slice(0,-1).join("");
+		}else if(e.key === "Enter" && cleanBtn.classList.contains("active")){
+			activeInput.value = "";
+		}else if(e.key === "Enter" && doneBtn.classList.contains("active")){
+			document.querySelector(".active-login").classList.remove("active-login");
+			loginBtn.classList.add("active-login");
+			keyboard.style.display = "none";
+		}
+	});
+}
+
+function printOnClick(){
+	const keys = document.querySelectorAll(".key");
+
+	if(document.querySelector(".active")){
+		document.querySelector(".active").classList.remove("active");
+	}
+	keys.forEach((item,index) => {
+
+		if(!item.classList.contains("shift") &&
+		   !item.classList.contains("back") &&
+		   !item.classList.contains("numbers-key") &&
+		   !item.classList.contains("done")&&
+		   !item.classList.contains("clean")){
+
+			item.addEventListener("click",(e) => {
+				
+
+				const activeInput = document.querySelector(".active-login");
+
+				if(document.querySelector(".active")){
+					document.querySelector(".active").classList.remove("active");
+				}
+				item.classList.add("active");
+
+				const rows = document.querySelectorAll(".row");
+				rows.forEach((item,rIndex) => {
+					
+					item.querySelectorAll(".key").forEach((key,kIndex) => {
+						if(key.classList.contains("active")){
+							
+							keyIndex = kIndex;
+							rowIndex = rIndex;
+						}
+
+					});
+				});
+
+				if(activeInput)activeInput.value += item.textContent;
+			});
+		   }
+		   else if(item.classList.contains("shift")){
+			item.addEventListener("click",() => {
+				const letters = document.querySelectorAll(".letter");
+				letters.forEach(item => {
+					if(item.textContent !== item.textContent.toUpperCase()){
+					item.textContent = item.textContent.toUpperCase();
+					}else{
+						item.textContent = item.textContent.toLowerCase();
+					}
+				});
+			});
+		   }
+		   else if(item.classList.contains("back")){
+			item.addEventListener("click",() => {
+				const activeInput = document.querySelector(".active-login");
+				activeInput.value = activeInput.value.split("").slice(0,-1).join("");
+			});
+		   }
+		   else if(item.classList.contains("clean")){
+			item.addEventListener("click", () => {
+				const activeInput = document.querySelector(".active-login");
+				activeInput.value = "";
+			});
+		   }
+		   else if(item.classList.contains("done")){
+			item.addEventListener("click",() => {
+				document.querySelector(".active-login").classList.remove("active-login");
+				loginBtn.classList.add("active-login");
+				keyboard.style.display = "none";
+			});
+		   }
+		   else if(item.classList.contains("numbers-key")){
+			if(item.textContent === "123"){
+				item.addEventListener("click",() => {
+					keyboard.innerHTML = "";
+					printKeyboard(numbersKeyboard);	
+					printOnClick();
+					keyIndex = 0;
+					rowIndex = 0;
+				});
+			}else{
+				item.addEventListener("click",() => {
+							keyboard.innerHTML = "";
+							printKeyboard(lettersKeyboard);	
+							printOnClick();
+							keyIndex = 0;
+							rowIndex = 0;
+				});
+							
+			}
+		   }
+		
+		   
+	});
+	if(document.querySelector(".active")){
+	document.querySelector(".active").classList.remove("active");
+	}
+
+}
+
+
 printKeyboard(lettersKeyboard);
+
 upLow();
 createNumbersKeyboard();
-activeKey();
 activeInputs();
+activeInputOnclick();
+print();
+printOnClick();
+deleteeOnKey();
 
